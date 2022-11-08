a1b692de97c14e0d1b4b2e4006bd5406b6228433
SHUBAAA <m.alvial03@ufromail.cl>
2022-11-08 06:56:19 -0300

    manolo

--- src/main/java/com/example/ProyectoInma/ProyectoInma.java
+++ src/main/java/com/example/ProyectoInma/ProyectoInma.java
@@ -18,6 +18,14 @@ public class ProyectoInma {
     }
 
 
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository usuarios, PasswordEncoder encoder) {
+        return args -> {
+
+            usuarios.save(new User("manolo@gmail.com", encoder.encode("a"), "11.222.333-4", "Manolo"));
+
+        };
+    }
 
 
 }
