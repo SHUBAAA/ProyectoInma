a1739fc1a47a0426736e439350a984b70a5a4e66
SHUBAAA <m.alvial03@ufromail.cl>
2022-11-15 15:35:59 -0300

    agrega a manolo y roles

--- src/main/java/com/example/ProyectoInma/ProyectoInma.java
+++ src/main/java/com/example/ProyectoInma/ProyectoInma.java
@@ -1,25 +1,36 @@
 package com.example.ProyectoInma;
 
+
 import com.example.ProyectoInma.Model.Rol;
 import com.example.ProyectoInma.Model.User;
-
 import com.example.ProyectoInma.Repository.RepoRol;
 import com.example.ProyectoInma.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+//DataSQL en resources
 @SpringBootApplication
 public class ProyectoInma {
+
+    @Autowired
+    RepoRol roleRepo;
     public static void main(String[] args) {
         SpringApplication.run(ProyectoInma.class, args);
     }
 
-    @Bean     CommandLineRunner commandLineRunner(UserRepository usuarios, PasswordEncoder encoder) {
-        return args -> {//usuarios.save(new User("manolo@gmail.com", encoder.encode("a"), "11.222.333-4", "Manolo"));
+    @Bean
+    CommandLineRunner commandLineRunner(RepoRol roles,UserRepository usuarios, PasswordEncoder encoder) {
+        return args -> {
+            roles.save(new Rol("ADMIN"));
+            roles.save(new Rol("CAJERO"));
+            roles.save(new Rol("BODEGA"));
+            usuarios.save(new User("manolo@gmail.com", encoder.encode("a"), "11.222.333-4", "Manolo"));
 
-    };     }
+        };
 
+
+    }
 }
